55669200186aefcfa19ff8c27db555dd4563dc50
Denislav <training.divanov@gmail.com>
2019-09-11 14:10:28 -0600

    secondChange

--- src/Config/coms.js
+++ src/Config/coms.js
@@ -1,1 +1,2 @@
-export const LIST_API = process.env.REACT_APP_LIST_API || "http://localhost:3000"
+//export const LIST_API = process.env.REACT_APP_LIST_API || "http://localhost:3000"
+export const LIST_API = 'https://portfolio-api-first.herokuapp.com';
